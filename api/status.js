605521b510dfbacb04e737aa06faf1f58dc1cc23
socialboosthub <muxidinxamza@gmail.com>
2026-08-30 12:05:56 +0300

    Rename status.js to api/status.js

---
+++ api/status.js
@@ -0,0 +1,1 @@
+export default async function handler(req,res){return res.status(200).json({service:'AHM Studio',version:'2',gpuConnected:false})}
--- status.js
+++
@@ -1,1 +0,0 @@
-export default async function handler(req,res){return res.status(200).json({service:'AHM Studio',version:'2',gpuConnected:false})}
